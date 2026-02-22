9e5b37f9ef66decb457397b622c62d05c43a2edc
dttric <87319664+dttric@users.noreply.github.com>
2026-02-22 14:32:24 +0700

    СУПЕРПУПЕР ГЛОАБЛЬНОЕ ОБНОВЛЕНИЕ САЙТА

--- nicknamechange.js
+++ nicknamechange.js
@@ -1,5 +1,4 @@
 let nameIndex = 0;
-let mobilenameIndex = 0;
 let nicknames = ["dttric", "cайлент", "toadtg"];
 
 function changeNameAutomatically() {
@@ -9,12 +8,4 @@ function changeNameAutomatically() {
     nameIndex = (nameIndex + 1) % nicknames.length;
 }
 
-function changemobileNameAutomatically() {
-    $("#mobilenicknamechange").fadeOut(500, function() {
-        $(this).text(nicknames[mobilenameIndex]).fadeIn(500);
-    });
-    mobilenameIndex = (mobilenameIndex + 1) % nicknames.length;
-}
-
-setInterval(changemobileNameAutomatically, 1700);
 setInterval(changeNameAutomatically, 1700);
